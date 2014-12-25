31b4ac39e0bbde15261cca0fade90fcc90c53484
HanyuX <xuehanyu@Hans-MacBook-Pro.local>
2014-12-25 11:30:06 -0800

    update 3D

--- main3D.js
+++ main3D.js
@@ -149,7 +149,7 @@
 
       function generateCube(nCubes, doMerge, generated){
         var img1 = new Image();//document.getElementById('myImg')
-        img1.src = "file:///Users/xuehanyu/Documents/water.png";
+        img1.src = "water.png";
         var canvas = document.createElement('canvas');
         var context = canvas.getContext('2d');
         canvas.width = img1.width;
